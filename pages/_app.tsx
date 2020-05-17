e38904d70cdcd76fa3e132c2333d83739123687a
Anish Aggarwal <anish17122000@gmail.com>
2020-05-17 18:52:43 +0530

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,48 @@
+import App from "next/app";
+import Router from "next/router";
+import NProgress from "nprogress";
+import { DefaultSeo } from "next-seo";
+// import {} from "dotenv";
+
+const DEFAULT_SEO = {
+  title: "Blog Tutorial Web",
+  description: "Awesome blog tutorial website",
+  openGraph: {
+    type: "website",
+    locale: "en",
+    title: "Blog Tutorial website",
+    description: "Awesome blog tutorial website",
+    site_name: "BlogTutorial",
+  },
+};
+
+export default class CustomApp extends App {
+  componentDidMount() {
+    Router.events.on("routeChangeComplete", () => {
+      NProgress.start();
+    });
+
+    Router.events.on("routeChangeComplete", () => {
+      NProgress.done();
+    });
+    Router.events.on("routeChangeError", () => {
+      NProgress.done();
+    });
+  }
+
+  componentDidCatch(error: any, errorInfo: any) {
+    console.log(error);
+    super.componentDidCatch(error, errorInfo);
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <>
+        <DefaultSeo {...DEFAULT_SEO} />
+        <Component {...pageProps} />;
+      </>
+    );
+  }
+}
